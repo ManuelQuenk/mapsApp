4077b1340f35c9d49e359bfcd6595b3965fd6398
mullet <mullet@gmail.com>
2024-04-09 16:23:16 -0300

    added zoom functionality, works with + - buttons

--- src/app/maps/maps.module.ts
+++ src/app/maps/maps.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { MapsRoutingModule } from './maps-routing.module';
+import mapboxgl from 'mapbox-gl';// or "const mapboxgl = require('mapbox-gl');"
+(mapboxgl as any).accessToken = "pk.eyJ1IjoibXVsbGV0bWFudSIsImEiOiJjbHVzaWp3Z3EwajE5Mmpua3Y1Nm00NWIzIn0.BXH02qD27MpXTxHmKUcGfQ";
+
+
 import { FullScreenPageComponent } from './pages/full-screen-page/full-screen-page.component';
-import { MarkerPageComponent } from './pages/marker-page/marker-page.component';
-import { ZoomPageComponent } from './pages/zoom-page/zoom-page.component';
-import { PropertiesPageComponent } from './pages/properties-page/properties-page.component';
-import { MiniMapComponent } from './components/mini-map/mini-map.component';
-import { SideMenuComponent } from './components/side-menu/side-menu.component';
 import { LayoutPageComponent } from './layout/layout-page/layout-page.component';
+import { MapsRoutingModule } from './maps-routing.module';
+import { MarkerPageComponent } from './pages/marker-page/marker-page.component';
+import { MiniMapComponent } from './components/mini-map/mini-map.component';
+import { PropertiesPageComponent } from './pages/properties-page/properties-page.component';
+import { SideMenuComponent } from './components/side-menu/side-menu.component';
+import { ZoomPageComponent } from './pages/zoom-page/zoom-page.component';
 
 
 @NgModule({
--- src/app/maps/pages/zoom-page/zoom-page.component.ts
+++ src/app/maps/pages/zoom-page/zoom-page.component.ts
@@ -1,9 +1,49 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { Map } from 'mapbox-gl';
 
 @Component({
   templateUrl: './zoom-page.component.html',
   styleUrl: './zoom-page.component.css'
 })
-export class ZoomPageComponent {
+export class ZoomPageComponent implements AfterViewInit{
+
+  @ViewChild('map') divMap?: ElementRef;
+
+  public zoom: number = 10;
+  public map?: Map;
+
+  ngAfterViewInit(): void {
+    if ( !this.divMap ) throw 'No elementRef found'
+
+    this.map = new Map({
+      container: this.divMap.nativeElement,
+      style: 'mapbox://styles/mapbox/streets-v12',
+      center:[-64, -31],
+      zoom: this.zoom,
+    });
+
+    this.mapListener()
+  }
+
+  mapListener() {
+    if (!this.map) throw 'Mapa malo'
+
+    this.map.on('zoom', (e) => {
+      this.zoom = this.map!.getZoom();
+    })
+
+    this.map.on('zoomend', (e) => {
+      if ( this.map!.getZoom() < 18 ) return;
+      this.map!.zoomTo(18)
+    })
+  }
+
+  zoomIn() {
+    this.map?.zoomIn();
+  }
+
+  zoomOut() {
+    this.map?.zoomOut();
+  }
 
 }
